6162b22ecd4ca139b7337739b63b3d20200aa3e3
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-07 20:16:39 +0530

    Edit button routing to edit page

---
+++ app/edit/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen flex flex-col items-center justify-center bg-black text-white px-4'>
+        <div className='w-full max-w-md border-2 border-white rounded-2xl p-8 shadow-lg'>
+
+        </div>
+    </div>
+  )
+}
+
+export default page
