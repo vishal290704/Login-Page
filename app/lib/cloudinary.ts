2ff2c5d6efe6904ca9d9284f09bb4e0767ecc070
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-10 20:17:58 +0530

    Cloudinary export complete

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -15,20 +15,22 @@ const uploadOnCloudinary = async (
     const arrayBuffer = await file?.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     return new Promise((resolve, reject) => {
-       const uploadStream = cloudinary.uploader.upload_stream({
-            resource_type:"auto"
-
-        },(error,result)=>{
-            if(error){
-                reject(error)
-            }else{
-                resolve(result?.secure_url ?? null)
-            }
-        
-        })
-        uploadStream.end(buffer)
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          resource_type: "auto",
+        },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result?.secure_url ?? null);
+          }
+        },
+      );
+      uploadStream.end(buffer);
     });
   } catch (error) {
     return null;
   }
 };
+export default uploadOnCloudinary;
