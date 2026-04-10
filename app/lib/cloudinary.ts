e8817a54afada356327f0a508300493e5449b222
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-10 19:43:56 +0530

    Cloudinary and enc configuration done

--- app/lib/cloudinary.ts
+++ app/lib/cloudinary.ts
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary'
 cloudinary.config({ 
-  cloud_name: 'my_cloud_name', 
-  api_key: 'my_key', 
-  api_secret: 'my_secret'
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
 });
