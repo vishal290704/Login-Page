9b4e6bd78c5ee8a08499315f186327423911c319
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-11 12:03:04 +0530

    Post api

--- app/api/edit/route.ts
+++ app/api/edit/route.ts
@@ -1,9 +1,38 @@
+import uploadOnCloudinary from "@/app/lib/cloudinary";
 import connectDB from "@/app/lib/db";
-import { NextRequest } from "next/server";
+import User from "@/app/model/user.model";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest) {
     try {
-        await connectDB
+        await connectDB()
+        const session = await getServerSession()
+        if(!session || !session.user.email || !session.user.id){
+            return NextResponse.json(
+                {message:"user doesn't have session"},
+                {status:400}
+            )
+        }
+        const formData = await req.formData()
+        const name = formData.get("name") as string
+        const file = formData.get("file") as Blob | null
+        let imageUrl = session.user.image ?? null
+
+        if(file){
+            imageUrl =await uploadOnCloudinary(file)
+        }
+
+        const user = await User.findByIdAndUpdate(session.user.id,{
+            name, image:imageUrl
+        })
+
+        if(!user){
+            return NextResponse.json(
+                {message:"user doesn't have session"},
+                {status:400}
+            )
+        }
     } catch (error) {
         
     }
