563f29c0411ad54d55c3747acadd700710578231
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-13 20:04:59 +0530

    GET API creating

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -5,5 +5,17 @@ import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req:NextRequest) {
-  
+    try {
+        await connectDB()
+        const session = await getServerSession(authOptions)
+            if(!session || !session.user.email || !session.user.id){
+                   return NextResponse.json(
+                       {message:"user doesn't have session"},
+                       {status:400}
+                   )
+               }
+               const user = await User.findById(session.user.id).select("-password")
+    } catch (error) {
+        
+    }
 }
