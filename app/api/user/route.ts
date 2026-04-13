eb081a00c93289b40984a2a33f98bd9de53f0ffb
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-13 20:10:25 +0530

    try part almost complete with return

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -4,18 +4,25 @@ import User from "@/app/model/user.model";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest) {
-    try {
-        await connectDB()
-        const session = await getServerSession(authOptions)
-            if(!session || !session.user.email || !session.user.id){
-                   return NextResponse.json(
-                       {message:"user doesn't have session"},
-                       {status:400}
-                   )
-               }
-               const user = await User.findById(session.user.id).select("-password")
-    } catch (error) {
-        
+export async function GET(req: NextRequest) {
+  try {
+    await connectDB();
+    const session = await getServerSession(authOptions);
+    if (!session || !session.user.email || !session.user.id) {
+      return NextResponse.json(
+        { message: "user doesn't have session" },
+        { status: 400 },
+      );
     }
+    const user = await User.findById(session.user.id).select("-password");
+    if (!user) {
+      return NextResponse.json(
+        { message: "user not found" }, 
+        { status: 400 });
+    }
+    return NextResponse.json(
+        user,
+        {status:200}
+    )
+  } catch (error) {}
 }
