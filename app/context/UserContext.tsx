0573ea02194ba6669036fbe9518d41a9721051ef
Vishal Srivastava <vsjee2002@gmail.com>
2026-04-24 16:52:01 +0530

    Context

--- app/context/UserContext.tsx
+++ app/context/UserContext.tsx
@@ -1,19 +1,30 @@
 import axios from 'axios'
-import React, { ReactNode, useEffect } from 'react'
-type userContextType{
-
+import { error } from 'console'
+import React, { ReactNode, useEffect, useState } from 'react'
+type userContextType={
+user:userType,
+setUser:(user:userType)=>void
 }
-
+type userType={
+    name:string,
+    email:string,
+    id:string,
+    image?:string
+}
 const userDataContext = React.createContext<userContextType | undefined>(undefined)
 const UserContext = ({children}:{children:ReactNode}) => {
+    const [user, setUser] = useState<userType>()
     useEffect(() => {
    async function getUser() {
     try {
         const result = await axios.get("/api/user")
-    } catch (error) {
+        setUser(result.data)
         
+    } catch (error) {
+        console.log(error)
     }
    }
+   getUser()
     }, [])
     
   return (
